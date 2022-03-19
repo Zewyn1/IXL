f9179bcc8a3ba5e2e398586a9d50e66f87d6c97d
Zewyn1 <danail.atanasov33@gmail.com>
2022-03-19 13:50:14 +0200

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          <img
+            src="https://drive.google.com/file/d/1tiRJBmfnSTCiU7VFLIEHiO8D2E_jGFmJ/view?usp=sharing"
+            alt="empty"
+          />
+        </a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
