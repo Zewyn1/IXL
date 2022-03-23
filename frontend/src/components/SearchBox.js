4b067803ae9c1a7ad273c6c3ce065223bfb07546
Zewyn1 <danail.atanasov33@gmail.com>
2022-03-23 23:18:24 +0200

    Add customer product reviews and slight UI change

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
@@ -14,21 +15,28 @@ export default function SearchBox() {
   };
 
   return (
-    <Form className="d-flex me-auto" onSubmit={submitHandler}>
-      <InputGroup>
-        <FormControl
-          type="text"
-          name="q"
-          id="q"
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="search products..."
-          aria-label="Search Products"
-          aria-describedby="button-search"
-        ></FormControl>
-        <Button variant="outline-primary" type="submit" id="button-search">
-          <i className="fas fa-search"></i>
-        </Button>
-      </InputGroup>
-    </Form>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        <img src="/Image/search.png" />
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Form className="d-flex me-auto" onSubmit={submitHandler}>
+          <InputGroup>
+            <FormControl
+              type="text"
+              name="q"
+              id="q"
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="search"
+              aria-label="Search Products"
+              aria-describedby="button-search"
+            ></FormControl>
+            <Button variant="outline-primary" type="submit" id="button-search">
+              <i className="fas fa-search"></i>
+            </Button>
+          </InputGroup>
+        </Form>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
